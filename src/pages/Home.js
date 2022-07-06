79db0c2345c30911d9cc23543784ea6a49497ff0
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-07-06 17:50:16 +0200

    👌 IMPROVE: fonctionnalités de base :
    - gestion des fetch
    - ternaire pour l'affichage de l'image
    - mise en place d'un loader pour le useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import CardMovie from '../components/CardMovie'
-/* import Movies from '../components/Movies' */
 import SearchBar from '../components/SearchBar'
 import axios from 'axios'
 import CardThumbnail from '../components/CardThumbnail'
 
 export default function Home() {
   const [popMovies, setPopMovies] = useState()
-  /* const [search, setSearch] = useState([]) */
+  const [search, setSearch] = useState("a")
+  const [movieData, setMovieData] = useState()
 
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`).then((res) => setPopMovies(res.data.results))
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${search}&language=fr-FR`).then((res) => setMovieData(res.data.results))
+  }, [search])
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=fr-FR`).then((res) => setPopMovies(res.data.results))
   }, [])
 
-  /* useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=a${search}`).then((res) => setSearch(res.data.results))
-  }, []) */
+  if (movieData === undefined) {
+    return <p>Loading...</p>
+  }
+
   if (popMovies === undefined) {
     return <p>Loading...</p>
   }
 
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  /* const movieView = movieData[0] === undefined ? popMovies[0] : movieData[0] */
+
   return (
     <div className="container">
-      <SearchBar />
+      <SearchBar handleChange={handleChange} />
       <div className="container-main">
-        <h2>Description</h2>
-        <CardMovie />
-        <h2>Recherche</h2>
-        {/* <Movies data={search} /> */}
-        <h2>Films populaires</h2>
+        <h2 style={{ textAlign: 'center', margin: '0.5em 0' }}>Description</h2>
+        <CardMovie data={popMovies[0]} />
+        {(search.length > 0) ? (
+          <>
+            <h2 style={{ textAlign: 'center', margin: '0.5em 0' }}>Recherche</h2>
+            <CardThumbnail data={movieData} />
+          </>
+        ) : null}
+        <h2 style={{ textAlign: 'center', margin: '0.5em 0' }}>Films populaires</h2>
         <CardThumbnail data={popMovies} />
       </div>
     </div>
