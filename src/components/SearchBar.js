b860ad4f933c6ce9f00687f1abc53a361d7eef9b
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-07-06 17:47:02 +0200

    🐛 FIX: correction de la barre de recherche

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,15 @@
 import styles from '../styles/SearchBar.module.css'
-import CardMovie from './CardMovie'
 
-export default function SearchBar() {
+
+export default function SearchBar({ handleChange }) {
   return (
     <div className={styles.searchBarComponent}>
       <form className={styles.searchBar}>
-        <input type='text' className={styles.input} placeholder="Rechercher un film" />
+        <input type='text'
+          className={styles.input}
+          placeholder="Rechercher un film"
+          onChange={handleChange}
+        />
         <input type="submit" className={styles.searchBtn} value="Rechercher" />
       </form>
     </div>
